428acf0f978a144c8de30bd0fde848e0b89f9981
pritesh <pritesh.vawsum@gmail.com>
2018-04-21 20:37:29 +0100

    Implemented Register API

--- IOChat/Controller/CreateAccountVC.swift
+++ IOChat/Controller/CreateAccountVC.swift
@@ -10,12 +10,36 @@ import UIKit
 
 class CreateAccountVC: UIViewController {
 
+    @IBOutlet weak var userNameTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passTxt: UITextField!
+    @IBOutlet weak var userImg: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func createAccountPressed(_ sender: Any) {
+        guard let email = emailTxt.text , emailTxt.text != "" else {
+            return
+        }
+        guard let pass = passTxt.text , passTxt.text != "" else {
+            return
+        }
+        
+        AuthService.instance.registerUser(email: email, password: pass) { (success) in
+            if success{
+                print("Registered User")
+            }
+        }
+    }
+    @IBAction func pickAvatarPressed(_ sender: UIButton) {
+        
+    }
     @IBAction func closedPressed(_ sender: UIButton) {
         performSegue(withIdentifier: UNWIND, sender: nil)
     }
