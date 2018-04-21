89ec03d2aa8ba6f713aba4340d03e42b2097b39a
pritesh <pritesh.vawsum@gmail.com>
2018-04-21 17:12:52 +0100

    Completed ChannelVC

---
+++ IOChat/View/GradientView.swift
@@ -0,0 +1,33 @@
+//
+//  GradientView.swift
+//  IOChat
+//
+//  Created by Pritesh Patel on 21/04/18.
+//  Copyright © 2018 COMP47390-41550. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class GradientView: UIView {
+
+    @IBInspectable var topColor: UIColor = #colorLiteral(red: 0.2901960784, green: 0.3019607843, blue: 0.8470588235, alpha: 1){
+        didSet{
+            self.setNeedsLayout()
+        }
+    }
+    @IBInspectable var bottomColor: UIColor = #colorLiteral(red: 0.1725490196, green: 0.831372549, blue: 0.8470588235, alpha: 1){
+        didSet{
+            self.setNeedsLayout()
+        }
+    }
+    override func layoutSubviews() {
+        // Creating a gradient and inserting it to the view
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors=[topColor.cgColor,bottomColor.cgColor]
+        gradientLayer.startPoint=CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint=CGPoint(x: 1, y: 1)
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at: 0)
+    }
+}
