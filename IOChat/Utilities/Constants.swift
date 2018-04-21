fb3e10cf944a603fa249fc5bf7eac88574521d11
pritesh <pritesh.vawsum@gmail.com>
2018-04-21 20:24:35 +0100

    Started impolementing Alimofire

--- IOChat/Utilities/Constants.swift
+++ IOChat/Utilities/Constants.swift
@@ -8,7 +8,19 @@
 
 import Foundation
 
+
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+//URL
+let BASE_URL = "https://iochat-ucd.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
 // Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+// UserDefaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
