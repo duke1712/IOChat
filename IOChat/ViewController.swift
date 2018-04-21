4411eb787ca9ffb3ea7a26bd3e7f8f5f07883b1e
pritesh <pritesh.vawsum@gmail.com>
2018-04-21 15:03:31 +0100

    Initial Commit

---
+++ IOChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOChat
+//
+//  Created by Pritesh Patel on 21/04/18.
+//  Copyright © 2018 COMP47390-41550. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
